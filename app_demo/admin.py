d5104fc2399db5c04d3558e6c2ab0e0edc3f898b
amol-tech <taj.amol@gmail.com>
2023-05-01 20:35:42 +0530

    Added employee list view

--- app_demo/admin.py
+++ app_demo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from app_demo.models import Department,Employee
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Employee)
--- app_demo/urls.py
+++ app_demo/urls.py
@@ -5,4 +5,5 @@ app_name = 'app_demo'
 
 urlpatterns = [
     path('main/', views.index_main, name='index_main'),
+    path('employees/', views.EmployeeListView.as_view(), name='index_employees'),
 ]
